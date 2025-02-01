b16502b46de63014f4cbbc8e2479ff1cee078bb1
pratap26pps <pankajpatna10321@gmail.com>
2025-02-02 00:43:38 +0530

    second commit

--- edtechserver/index.js
+++ edtechserver/index.js
@@ -19,7 +19,7 @@ require('./config/database').dbconnect();
 app.use(cookieparser());
 app.use(
     cors({
-      origin: 'https://edtechperception.vercel.app/', 
+      origin: 'https://edtechperception.vercel.app', 
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       allowedHeaders: ['Content-Type', 'Autharisation'], 
       credentials: true, 
