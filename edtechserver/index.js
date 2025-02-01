e9ffc69abe2d3ccf777b1513ac117db5e4615a80
pratap26pps <pankajpatna10321@gmail.com>
2025-02-02 03:12:08 +0530

    autharization

--- edtechserver/index.js
+++ edtechserver/index.js
@@ -21,7 +21,7 @@ app.use(
     cors({
       origin: 'https://edtechperception.vercel.app', 
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      allowedHeaders: ['Content-Type', 'Authorization'], 
+      allowedHeaders: ['Content-Type', 'Autharisation'], 
       credentials: true, 
     }) );
   
