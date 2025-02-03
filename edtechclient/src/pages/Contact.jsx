bb8fa7474726efd26ecca1544c1d59b16badfb16
pratap26pps <pankajpatna10321@gmail.com>
2025-02-04 00:48:44 +0530

    form

--- edtechclient/src/pages/Contact.jsx
+++ edtechclient/src/pages/Contact.jsx
@@ -3,18 +3,8 @@ import Footer from './Footer';
 import { IoMdChatboxes } from "react-icons/io"; 
 import { GiWorld } from "react-icons/gi";
 import { IoMdCall } from "react-icons/io";
-import { toast } from 'react-toastify';
-import { useForm } from 'react-hook-form';
+ 
  const Contact=() =>{
-  const {register,handleSubmit,formState:{errors}}=useForm();
-  const messagesent =()=>{
-    toast.success("message sent")
-    return
-  }
-  const onSubmit = (data) => {
-    messagesent();
-    console.log(data);  // For debugging, remove in production
-  };
   
   return (
     <div className='text-white flex flex-col justify-center items-center mt-20'>
@@ -61,51 +51,51 @@ import { useForm } from 'react-hook-form';
        from-purple-600 via-pink-600  lg:text-3xl 
         to-blue-600 bg-gradient-to-r bg-clip-text text-transparent'>Got a Idea? We've got the skills. Let's team up</p>
                    <p>Tell us more about yourself and what you're got in mind.</p>
-                   <form  onSubmit={handleSubmit(onSubmit)} action="https://api.web3forms.com/submit" method="POST">
-                   
+                   <form action="https://api.web3forms.com/submit" method="POST">
+                   <input type="hidden" name="access_key" value="ade29983-eceb-4438-8b57-45543132b11d"/>
                     <div className='flex gap-9 my-8'>
-                      <input type="hidden" name="access_key" value="ade29983-eceb-4438-8b57-45543132b11d" />
+              
                         <div className='flex flex-col'>
                           <label>first name</label>
-                          <input type="text" placeholder='Enter first name' id='firstname'
-                            {...register("firstname",{required:true})}
+                          <input type="text" placeholder='Enter first name' name='firstname' required
+                            
                            className='bg-slate-500 lg:p-3 rounded-md  text-black font-bold' />
-                         {errors.firstname  && (<span className='text-red-600'>first name is required</span>)}
+                          
                         </div>
                         <div className='flex flex-col'>
                           <label>last name</label>
-                          <input type="text" placeholder='Enter last name' id='lastname'
-                            {...register("lastname",{required:true})}
+                          <input type="text" placeholder='Enter last name' name='lastname'
+                    required
                            className='bg-slate-500 lg:p-3 rounded-md text-black font-bold' />
                             
-                             {errors.lastname && (<span className='text-red-600'>last name is required</span>)}
+                        
 
                         </div>
                     </div>
                     <div className='flex-col flex'>
                                 <label>Email Address</label>
-                     <input type="text" placeholder='email@gmail.com' id='email'
-                       {...register("email",{required:true})}
+                     <input type="text" placeholder='email@gmail.com' name='email'
+                     required
                          className='bg-slate-500 lg:p-3 rounded-md text-black font-bold' />
-                           {errors.email  && (<span className='text-red-600'>email is required</span>)}
+                          
                     </div>
           
                       <div className='flex flex-col'>
                                           <label>Phone number</label>
-                          <input type="number" placeholder='1234567890'
+                          <input type="number" placeholder='contact number' name='contactno'  required
                            className='bg-slate-500 lg:p-3 rounded-md text-black font-bold' /> 
                       </div>
 
                      <div className='flex flex-col'>
                           <label>Message</label>
                            <textarea placeholder='enter your message here '  name="message" 
-                             {...register("textarea",{required:true})}
+                            required
                             className='bg-slate-500 lg:p-3 h-80 rounded-md text-black font-bold'/>
-                              {errors.textarea  && (<span className='text-red-600'>text area is required</span>)}
+                           
                      </div>
 
-
-                     <button type='submit' className='bg-yellow-400 hover:bg-green-500
+                     <input type="checkbox" name="botcheck" class="hidden" style="display: none;"/>
+                     <button type="submit" className='bg-yellow-400 hover:bg-green-500
                      transition-all duration-300 my-6 p-5 rounded-lg hover:scale-95'>
                       send message
                       </button>
